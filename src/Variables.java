6c87f3b314368ebfb0a69a888fedf77e5d8022c7
itsluismario <itsluismario@gmail.com>
2024-08-23 18:03:11 -0600

    :tada: How to use Java 101
    In the first commit, it shows an example of how to use variables in Java

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declarando una variable
+        int speed;
+        // Asginamos un valor a una variable
+        speed = 10;
+        String employeeName = "Popo";
+        System.out.println(speed);
+        System.out.println(employeeName);
+    }
+}

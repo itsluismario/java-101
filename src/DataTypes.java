c9bdf0e7d7072537ac53acdbfdbd571b93a56fa9
itsluismario <itsluismario@gmail.com>
2024-08-24 18:57:32 -0600

    :sparkles: Add var keyword explanation to Java Data Types section
    - Introduce var keyword for type inference in Java
    - Provide example code demonstrating var usage
    - Explain how Java infers types with var
    - Highlight cases where var infers int and double types
    - Note limitations and best practices for using var

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -9,5 +9,14 @@ public class DataTypes {
         // Float
         float nF = 1.1234567890123456789123456789F;
         double nD = 1.123456789;
+
+        // var is a keyword that allows Java to infer the most appropriate data type based on the assigned value.
+        var salary = 10; // int
+        var pension = salary*0.03; // double
+        var totalSalary = salary - pension; // double
+
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
     }
 }
